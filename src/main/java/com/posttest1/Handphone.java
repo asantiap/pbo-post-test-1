897e23545d13f2d8b8b237d337656f6da8bf1faf
asantiap <sagitasantia@gmail.com>
2023-09-24 10:28:46 +0800

    postest1

---
+++ src/main/java/com/posttest1/Handphone.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.posttest1;
+
+/**
+ *
+ * @author anggr
+ */
+public class Handphone {
+    private String nama; //Ini adalah deklarasi variabel instance yang disebut name dengan tipe data String.Variabel ini tidak dapat diakses oleh kelas lain.
+    private int tahunRilis; //Ini adalah deklarasi variabel instance yang disebut tahunRilis dengan tipe data int (integer).
+    //kata kunci private mengindikasikan bahwa variabel tahunRilis hanya dapat diakses secara langsung oleh kode dalam kelas "Handphone".
+
+    public Handphone(String nama, int tahunRilis) { // Ini adalah konstruktor kelas "Handphone".
+        this.nama = nama;
+        this.tahunRilis = tahunRilis;
+    }
+
+    public String getNama() { //Ini adalah metode-metode getter (pengambil) yang digunakan untuk mengambil nilai variabel instance "nama" 
+        return nama;
+    }
+
+    public void setNama(String nama) { //: Ini adalah metode-metode penetap yang digunakan untuk mengubah nilai variabel instance "nama"
+        this.nama = nama;
+    }
+
+    public int getTahunRilis() { //Ini adalah metode-metode getter (pengambil) yang digunakan untuk mengambil nilai variabel instance "tahunrilis" 
+        return tahunRilis;
+    }
+
+    public void setTahunRilis(int tahunRilis) { //: Ini adalah metode-metode setter (penetap) yang digunakan untuk mengubah nilai variabel instance "tahunRilis"
+        this.tahunRilis = tahunRilis;
+    }
+}
---
+++ src/main/java/com/posttest1/Posttest1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.posttest1;
+
+    
+import java.util.ArrayList; //impor (import statement) yang digunakan untuk mengimpor kelas ArrayList
+
+public class Posttest1 { //Ini adalah deklarasi kelas utama yang disebut "Postest1".
+     public static void main(String[] args) { 
+        // ArrayList untuk Nama-nama Handphone
+        ArrayList<Handphone> daftarHandphone = new ArrayList<>();
+
+        // ArrayList dengan nama-nama Handphone
+        daftarHandphone.add(new Handphone("iPhone 15 Pro", 2023));
+        daftarHandphone.add(new Handphone("Samsung Galaxy S23", 2023));
+        daftarHandphone.add(new Handphone("Oppo Reno 10", 2024));
+        daftarHandphone.add(new Handphone("Poco F5 Pro", 2023));
+        daftarHandphone.add(new Handphone("Xiaomi Mi 11", 2021));
+
+        // Perulangan untuk menampilkan informasi Handphone
+        for (Handphone handphone : daftarHandphone) {
+            System.out.println("Handphone: " + handphone.getNama() + ", Tahun Rilis: " + handphone.getTahunRilis());
+        }
+    }
+}
